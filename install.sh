4494d90150d95083b007fdd87a66b7589eacd25d
Pixelsaft GbR <info@pixelsaft.de>
2020-05-14 16:22:45 +0200

    (fix) path with spaces should now work as aspected

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-ln -s $PWD/zshrc $HOME/.zshrc
-ln -s $PWD/gitconfig $HOME/.gitconfig
+ln -s "$PWD/zshrc" "$HOME/.zshrc"
+ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
 
 # read -p "Enter your github email address" email
 # read -p "Enter your github name" name
